19880d1225c52044cdd8f7115d3bb12cf2a92b99
Tawakalt Oyeleso <oyelesotawakalt@gmail.com>
2026-06-23 18:51:18 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from urllib.parse import quote_plus
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DB_URL")
 
 
-engine = create_engine(
-	DATABASE_URL,
-	pool_pre_ping = True)
+if not DATABASE_URL:
+    raise ValueError("DB_URL is missing from environment variables")
+
+engine = create_engine(DATABASE_URL)
