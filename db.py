bd82fdb28f2d50c8f578645eee1e5cddda1c93de
Tawakalt Oyeleso <oyelesotawakalt@gmail.com>
2026-06-24 20:40:20 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,23 +1,47 @@
 from sqlalchemy import text
 from logger import logger
 
+# -------------------------
+# SAVE CRYPTO DATA
+# -------------------------
 def save_to_db(engine, data):
     if not data:
         logger.warning("No data received for DB insert")
         return
 
     query = text("""
-        INSERT INTO crypto_prices (coin, price, market_cap)
-        VALUES (:coin, :price, :market_cap)
+        INSERT INTO crypto_prices (coin, price, market_cap, created_at)
+        VALUES (:coin, :price, :market_cap, NOW())
         ON CONFLICT DO NOTHING
     """)
 
     try:
         with engine.begin() as conn:
-            conn.execute(query, data)  # list of dicts is OK in SQLAlchemy 2.0+
+            conn.execute(query, data)
 
         logger.info(f"DB insert successful: {len(data)} records")
 
     except Exception as e:
         logger.error(f"DB insert failed: {e}")
         raise
+
+
+# -------------------------
+# SAVE PIPELINE LOGS
+# -------------------------
+def save_pipeline_log(engine, status, runtime, error_message):
+    query = text("""
+        INSERT INTO pipeline_runs (status, runtime_seconds, error_message)
+        VALUES (:status, :runtime, :error)
+    """)
+
+    try:
+        with engine.begin() as conn:
+            conn.execute(query, {
+                "status": status,
+                "runtime": runtime,
+                "error": error_message
+            })
+
+    except Exception as e:
+        logger.error(f"Pipeline log insert failed: {e}")
