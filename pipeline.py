ede35025424f3b56fcc367d99b1bdd7b59b8b1ff
Tawakalt Oyeleso <oyelesotawakalt@gmail.com>
2026-06-24 20:40:35 +0100

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,52 +1,33 @@
 import time
-import os
-from logger import logger
 from api import fetch_crypto
-from db import save_to_db
 from validation import validate_data
+from db import save_to_db, save_pipeline_log
 from config import engine
-
-LOCK_FILE = "pipeline.lock"
+from logger import logger
 
 
 def run_pipeline():
-
     start = time.time()
-    logger.info("Pipeline started")
 
     try:
+        logger.info("Pipeline started")
+
         raw_data = fetch_crypto()
         clean_data = validate_data(raw_data)
+
         save_to_db(engine, clean_data)
+
+        runtime = time.time() - start
+        save_pipeline_log(engine, "success", runtime, None)
 
         logger.info("Pipeline completed successfully")
 
     except Exception as e:
+        runtime = time.time() - start
+        save_pipeline_log(engine, "failed", runtime, str(e))
+
         logger.error(f"Pipeline crashed: {e}")
 
-    finally:
-        duration = time.time() - start
-        logger.info(f"Pipeline runtime: {duration:.2f}s")
-
-
-# -------------------------
-# LOCK WRAPPER (IMPORTANT)
-# -------------------------
 
 if __name__ == "__main__":
-
-    # 1. Check if already running
-    if os.path.exists(LOCK_FILE):
-        logger.warning("Pipeline already running. Exiting.")
-        exit()
-
-    # 2. Create lock file
-    open(LOCK_FILE, "w").close()
-
-    try:
-        run_pipeline()
-
-    finally:
-        # 3. Always remove lock
-        if os.path.exists(LOCK_FILE):
-            os.remove(LOCK_FILE)
+    run_pipeline()
