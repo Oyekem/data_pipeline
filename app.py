cc1779ba2d1ea041435abc3c4ea28dd6660ebeeb
Tawakalt Oyeleso <oyelesotawakalt@gmail.com>
2026-06-24 17:31:22 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Crypto Analytics Platform",
+    page_icon="📈",
+    layout="wide"
+)
+
+st.title("🚀 Crypto Analytics Platform")
+
+st.write("""
+Use the left sidebar to navigate:
+
+- Introduction
+- Analytics Dashboard
+- Prediction
+""")
