e32d7a804902be377b17e1ac31b5e9a960b85d4b
Tawakalt Oyeleso <oyelesotawakalt@gmail.com>
2026-06-30 18:31:12 +0100

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,23 +1,24 @@
 import logging
-from datetime import datetime
 import os
+from datetime import datetime
 
 os.makedirs("logs", exist_ok=True)
 
 log_file = f"logs/pipeline_{datetime.now().date()}.log"
 
 logger = logging.getLogger("crypto_pipeline")
-logger.setLevel(logging.INFO)
 
-if not logger.handlers:   # IMPORTANT FIX
-    file_handler = logging.FileHandler(log_file)
-    stream_handler = logging.StreamHandler()
+if not logger.handlers:
+    logger.setLevel(logging.INFO)
 
     formatter = logging.Formatter(
         "%(asctime)s | %(levelname)s | %(message)s"
     )
 
+    file_handler = logging.FileHandler(log_file)
     file_handler.setFormatter(formatter)
+
+    stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
 
     logger.addHandler(file_handler)
